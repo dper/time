e5e51f93d00b93707806d24d39e031f0358206f8
Douglas Perkins <webmaster@dperkins.org>
2015-11-09 18:20:22 +0900

    First commit.

---
+++ globe.php
@@ -0,0 +1,45 @@
+<?php
+
+// Returns nicely formatted date and time.
+function formattedTime() {
+	return "<div>" . date("H:i") . "</div>";
+}
+
+// Returns a table row showing timezone information.
+function timezone($place, $tz, $x, $y) {
+	$html = '<div style="position: absolute; top: ' . $y . 'px; left: ' . $x . 'px;">' . "\n";
+	$html .= "<div>" . $place . "</div>\n";
+	date_default_timezone_set($tz);
+	$html .= formattedTime();
+	$html .= "</div>\n";
+	return $html;
+}
+
+header("Content-Type: text/html");
+header("Expires: Tue, 01 Jan 1990 00:00:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+echo "<!DOCTYPE html>\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "<title>World Times</title>\n";
+echo "</head>\n";
+echo '<body style="text-align: center;">' . "\n";
+echo "<h1>World Times</h1>\n";
+
+echo '<div style="margin-left: auto; margin-right: auto; position: relative; background: url(\'world.svg\'); width: 1000px; height: 511px;">' . "\n";
+
+echo timezone("Seattle", "America/Los_Angeles", 30, 120);
+echo timezone("Los Alamos", "America/Denver", 55, 170);
+echo timezone("Grand Forks", "America/Chicago", 100, 115);
+echo timezone("Pittsburgh", "America/New_York", 175, 150);
+echo timezone("Norway", "Europe/Oslo", 470, 50);
+echo timezone("Tokyo", "Asia/Tokyo", 900, 170);
+
+echo "</div>\n";
+
+echo "</html>\n";
+echo "</body>\n";
+?>
---
+++ time.php
@@ -0,0 +1,57 @@
+<?php
+
+// Returns nicely formatted date and time.
+function formattedTime() {
+	return "<td>" . date("H:i") . "</td>";
+}
+
+// Returns nicely formatted date and time.
+function formattedDate() {
+	return "<td>" . date("D, d M Y") . "</td>";
+}
+
+// Returns a table row showing timezone information.
+function timezoneRow($place, $tz) {
+	$html = "<tr>\n";
+	$html .= "<th>" . $place . "</th>\n";
+	date_default_timezone_set($tz);
+	$html .= formattedTime();
+	$html .= formattedDate();
+	$html .= "</tr>\n";
+	return $html;
+}
+
+header("Content-Type: text/html");
+header("Expires: Tue, 01 Jan 1990 00:00:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+echo "<!DOCTYPE html>\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "<title>World Times</title>\n";
+echo "</head>\n";
+echo '<body style="text-align: center;">' . "\n";
+echo "<h1>World Times</h1>\n";
+
+echo '<table cellpadding="8" border="1" style="border-collapse: collapse; margin-left: auto; margin-right: auto; text-align: center;">' . "\n";
+
+echo "<tr>\n";
+echo "<th>Place</th>\n";
+echo "<th>Time</th>\n";
+echo "<th>Date</th>\n";
+echo "</tr>\n";
+
+echo timezoneRow("Seattle", "America/Los_Angeles");
+echo timezoneRow("Los Alamos", "America/Denver");
+echo timezoneRow("Grand Forks", "America/Chicago");
+echo timezoneRow("Pittsburgh", "America/New_York");
+echo timezoneRow("Norway", "Europe/Oslo");
+echo timezoneRow("Tokyo", "Asia/Tokyo");
+
+echo "</table>\n";
+
+echo "</html>\n";
+echo "</body>\n";
+?>
